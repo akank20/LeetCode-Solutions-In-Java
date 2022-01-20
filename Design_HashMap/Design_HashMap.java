10c286efddcf53aefb4195ed3713b22f77ab605c
Akanksha <36906392+akank20@users.noreply.github.com>
2022-01-19 18:03:48 -0800

    Solution-1

---
+++ Design_HashMap/Design_HashMap.java
@@ -0,0 +1,30 @@
+class MyHashMap {
+    Integer[] arr;
+    public MyHashMap() {
+        arr = new Integer[1000001];
+    }
+    
+    public void put(int key, int value) {
+       arr[key] = value;
+    }
+    
+    public int get(int key) {
+        if (arr[key] == null) {
+            return -1;
+        } else {
+            return arr[key];
+        }
+    }
+    
+    public void remove(int key) {
+        arr[key] = null;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
